dbf9069f309663860cebe6ca2f95ba6460b939c7
Mehloul Mohamed <m.mehloul@esi-sba.dz>
2024-06-20 15:18:28 +0100

    Better Implementation Of italize()

--- tools/italics.py
+++ tools/italics.py
@@ -1,9 +1,10 @@
-# Not a complete implementation. Still needs more work
 def italize(command):
     result=''
     for c in command:
         n=ord(c)
-        if (n in range(65,91)) or (n in range(97,123)):
+        if (n in range(65,91)):
+            result+=chr(119912+n-65)
+        elif (n in range(97,123)):
             result+=chr(119938+n-97)
         else:
             result+=c
